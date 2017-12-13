82890594844e4d265fe44096612ce528263c6210
Zhangzike <m15287298298@163.com>
2017-12-13 21:43:01 +0800

    Add chufa

---
+++ Scanner/src/Scanner/chufa.java
@@ -0,0 +1,18 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class TestScanner {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+       Scanner s=new Scanner(System.in);
+       System.out.println("请输入除数：");
+       int a=s.nextInt();
+       System.out.println("请输入被除数：");
+       int b=s.nextInt();
+       int sum=a/b;
+       System.out.println("计算结果，商为："+sum);
+	}
+
+}
